f9703a1f30832c397c1e1a8e400231214fc0ba8a
Ed Knutson <knutsoned+github@gmail.com>
2024-03-04 10:29:35 -0600

    remove unused BJS stuff

--- src/createScene.ts
+++ src/createScene.ts
@@ -3,11 +3,9 @@ import type { Scene } from "@babylonjs/core/scene";
 
 // Change this import to check other scenes
 //import { DefaultSceneWithTexture } from "./scenes/defaultWithTexture";
-//import { FresnelShaderScene } from "./scenes/fresnelShader";
 //import { LoadModelAndEnvScene } from "./scenes/loadModelAndEnv";
-//import { NavigationMeshRecast } from "./scenes/navigationMeshRecast";
+import { NavigationMeshRecast } from "./scenes/navigationMeshRecast";
 //import { PhysicsSceneWithAmmo } from "./scenes/physicsWithAmmo";
-import { PhysicsSceneWithHavok } from "./scenes/physicsWithHavok";
 
 export interface CreateSceneClass {
     createScene: (engine: Engine, canvas: HTMLCanvasElement) => Promise<Scene>;
@@ -20,9 +18,7 @@ export interface CreateSceneModule {
 
 export const getSceneModule = (): CreateSceneClass => {
     //return new DefaultSceneWithTexture();
-    //return new FresnelShaderScene();
     //return new LoadModelAndEnvScene();
-    //return new NavigationMeshRecast();
+    return new NavigationMeshRecast();
     //return new PhysicsSceneWithAmmo();
-    return new PhysicsSceneWithHavok();
 };
